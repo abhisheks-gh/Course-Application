1574582c2abafd7fb88dd99ff8810a68db6e51ae
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-03-22 19:25:51 +0530

    Add updateCourse() & deleteCourse() method
    Add updateCourse() & deleteCourse() method using PUT and DELETE HTTP methods respectively.
    Also, deleteCourse() method is returning a ResponseEntity object of generic type HttpStatus

--- Spring-REST/src/main/java/com/springrest/springrest/Controller/MyController.java
+++ Spring-REST/src/main/java/com/springrest/springrest/Controller/MyController.java
@@ -1,8 +1,13 @@
 package com.springrest.springrest.Controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,4 +49,20 @@ public class MyController {
         return this.courseService.addCourse(course);
     }
 
+    /** Update Course using PUT request */
+    @PutMapping("/courses")
+    public Course updateCourse(@RequestBody Course course) {
+        return this.courseService.updateCourse(course);
+    }
+
+    /** Delete course by ID */
+    @DeleteMapping("/courses/{courseId}")
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId) {
+        try {
+            this.courseService.deleteCourse(Long.parseLong(courseId));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
