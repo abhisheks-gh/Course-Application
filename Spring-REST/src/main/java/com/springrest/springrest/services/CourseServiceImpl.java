60f5734f726d40d9e41112568023d258292e8feb
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2023-03-22 19:27:20 +0530

    Update CourseServiceImpl.java

--- Spring-REST/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ Spring-REST/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -3,6 +3,7 @@ package com.springrest.springrest.services;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
 
 import com.springrest.springrest.entities.Course;
@@ -10,8 +11,6 @@ import com.springrest.springrest.entities.Course;
 
 @Service        // This class is working in the service layer
 public class CourseServiceImpl implements CourseService {
-
-
     List<Course> list;
 
     public CourseServiceImpl() {
@@ -41,6 +40,24 @@ public class CourseServiceImpl implements CourseService {
     public Course addCourse(Course course) {
         list.add(course);
         return course;
+    }
+
+    @Override
+    public Course updateCourse(Course course) {
+        list.forEach(e -> {
+            if (e.getId() == course.getId()) {
+                e.setTitle(course.getTitle());
+                e.setDescription(course.getDescription());
+            }
+        });
+
+        return course;
+    }
+
+    public void deleteCourse(long parseLong) {
+        // Extracting a stream from the list and filtering all ID's that don't match the passed ID as a list.
+        // This way the passed ID will be deleted.
+        list = list.stream().filter(e -> e.getId() != parseLong).collect(Collectors.toList());
     }
 
 }
